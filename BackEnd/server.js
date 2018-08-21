545da3a9c289570d0b00cb2751d7fe6f42655d5a
Ashutosh Singh <ashutosh.singh@sjsu.edu>
2018-08-21 10:59:47 -0700

    Added helmet. Updated packages

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,14 +1,18 @@
 const config = require('./config/config');
 const express = require('express'); 
-const app=express();
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const AuthorizationRouter = require('./routes/routes.config');
 const UsersRouter = require('./routes/users_routes');
 
+const app=express();
+
+app.use(helmet());
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DEconstE');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST');
     res.header('Access-Control-Expose-Headers', 'Content-Length');
     res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
     if (req.method === 'OPTIONS') {
